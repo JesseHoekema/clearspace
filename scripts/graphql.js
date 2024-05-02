5cc0d679af173753e1b39aa03d1b9cbc0d10a21b
Daan <20974756+daanschenkel@users.noreply.github.com>
2024-05-02 12:24:03 +0200

    Improve errors even more :D

--- scripts/graphql.js
+++ scripts/graphql.js
@@ -1,3 +1,10 @@
+class GraphQLError extends Error {
+    constructor(errors) {
+        super('GraphQL Error');
+        this.errors = errors;
+    }
+}
+
 export async function query(query, variables = {}) {
     const response = await fetch('http://localhost:4000/graphql', {
         method: 'POST',
@@ -9,9 +16,7 @@ export async function query(query, variables = {}) {
 
     if (errors?.length > 0) {
         //return error with multiple attributes
-        throw new Error(errors.map(({ message, locations, path }) => {
-            return `${message} at ${locations.map(({ line, column }) => `line ${line}, column ${column}`).join(', ')}, in ${path}`;
-        }).join('\n'));
+        throw new GraphQLError(errors);
     }
 
     return data;
