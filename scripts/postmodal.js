ede5181cd9a436748761d8a277cd375a6f293902
bruce10panda <vandengoorb@gmail.com>
2024-05-08 15:25:25 +0200

    added edit profile

--- scripts/postmodal.js
+++ scripts/postmodal.js
@@ -1,27 +1,33 @@
-  // JavaScript code
-  // Get the modal element
-  var modal = document.getElementById("modal");
-  var targetDiv = document.getElementById("blur");
+// Get the modal element
+var modal = document.getElementById("modal");
 
-  // Get the button that opens the modal
-  var btn = document.getElementById("openModalBtn");
+// Get the button that opens the modal inside the sidebar
+var btn = document.getElementById("openModalBtn");
 
-  // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
+// Get the button that opens the modal outside the sidebar
+var btn2 = document.getElementById("openModalBtn2");
 
-  // When the user clicks the button, open the modal 
-  btn.onclick = function() {
-    modal.style.display = "flex";
-  }
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
+// When the user clicks the button inside the sidebar, open the modal 
+btn.onclick = function() {
+  modal.style.display = "flex";
+}
+
+// When the user clicks the button outside the sidebar, open the modal 
+btn2.onclick = function() {
+  modal.style.display = "flex";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
     modal.style.display = "none";
   }
-
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  }
+}
